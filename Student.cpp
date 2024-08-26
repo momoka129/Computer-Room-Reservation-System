94cf20effcce2349c02a0187eb56416b1746f8f6
Kinoko <huiyuanai129@outlook.com>
2024-08-26 10:31:56 +0800

    Set up the framework for the 'Student' class

---
+++ Student.cpp
@@ -0,0 +1,40 @@
+//
+// Created by 24101 on 2024/8/26.
+//
+
+#include "Student.h"
+
+Student::Student(){
+
+}
+
+//constructor with parameters
+Student::Student(int id, string name, string pwd)
+{
+
+}
+
+//student menu
+void Student::each_Menu(){
+
+}
+
+//apply for a reservation
+void Student::apply_R(){
+
+}
+
+//check own reservations
+void Student::view_my_R(){
+
+}
+
+//check all reservation
+void Student::view_all_R(){
+
+}
+
+//cancel reservation
+void Student::cancel_R(){
+
+}
---
+++ Student.h
@@ -0,0 +1,41 @@
+//
+// Created by 24101 on 2024/8/26.
+//
+
+#ifndef COMPUTER_ROOM_RESERVATION_SYSTEM_STUDENT_H
+#define COMPUTER_ROOM_RESERVATION_SYSTEM_STUDENT_H
+
+#pragma once
+#include <iostream>
+using namespace std;
+#include "Identity.h"
+
+class Student : public Identity{
+public:
+    Student();
+
+    //constructor with parameters
+    Student(int id, string name, string pwd);
+
+    //student menu
+    virtual void each_Menu();
+
+    //apply for a reservation
+    void apply_R();
+
+    //check own reservations
+    void view_my_R();
+
+    //check all reservation
+    void view_all_R();
+
+    //cancel reservation
+    void cancel_R();
+
+
+    int ID;     //student id
+
+};
+
+
+#endif //COMPUTER_ROOM_RESERVATION_SYSTEM_STUDENT_H
