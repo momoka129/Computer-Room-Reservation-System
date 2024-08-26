4d3bb4a69af7ee023293255a3dc5569446e4fb11
Kinoko <huiyuanai129@outlook.com>
2024-08-26 10:47:53 +0800

    Built the structure for the 'Admin' class

---
+++ Admin.cpp
@@ -0,0 +1,38 @@
+//
+// Created by 24101 on 2024/8/26.
+//
+
+#include "Admin.h"
+
+Admin::Admin(){
+
+}
+
+Admin::Admin(string name, string pwd){
+
+}
+
+//Admin menu
+void Admin::each_Menu(){
+
+}
+
+//add account
+void Admin::add_Acc(){
+
+}
+
+//view accounts
+void Admin::view_acc(){
+
+}
+
+//view computer room info
+void Admin::view_CR_info(){
+
+}
+
+//clear reservation records
+void Admin::clear_file(){
+
+}
---
+++ Admin.h
@@ -0,0 +1,37 @@
+//
+// Created by 24101 on 2024/8/26.
+//
+
+#ifndef COMPUTER_ROOM_RESERVATION_SYSTEM_ADMIN_H
+#define COMPUTER_ROOM_RESERVATION_SYSTEM_ADMIN_H
+
+#pragma once
+#include <iostream>
+using namespace std;
+#include "Identity.h"
+
+
+class Admin : public Identity{
+public:
+    Admin();
+
+    Admin(string name, string pwd);
+
+    //Admin menu
+    virtual void each_Menu();
+
+    //add account
+    void add_Acc();
+
+    //view accounts
+    void view_acc();
+
+    //view computer room info
+    void view_CR_info();
+
+    //clear reservation records
+    void clear_file();
+};
+
+
+#endif //COMPUTER_ROOM_RESERVATION_SYSTEM_ADMIN_H
