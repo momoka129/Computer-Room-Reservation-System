db98a7d1a93cecfcc597c68319296996ec4fc36c
Kinoko <huiyuanai129@outlook.com>
2024-08-27 10:19:13 +0800

    Add global file

---
+++ globalFile.h
@@ -0,0 +1,20 @@
+//
+// Created by 24101 on 2024/8/27.
+//
+
+#ifndef COMPUTER_ROOM_RESERVATION_SYSTEM_GLOBALFILE_H
+#define COMPUTER_ROOM_RESERVATION_SYSTEM_GLOBALFILE_H
+
+#pragma once
+
+#define ADMIN_FILE  "admin.txt"
+
+#define STUDENT_FILE    "student.txt"
+
+#define TEACHER_FILE    "teacher.txt"
+
+#define COMPUTER_FILE   "computerRoom.txt"
+
+#define RESERVATION_FILE    "reservation.txt"
+
+#endif //COMPUTER_ROOM_RESERVATION_SYSTEM_GLOBALFILE_H
